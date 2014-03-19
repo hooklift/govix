4d55a60475c5b24ab618d139ee242a415977ea15
Camilo Aguilar <camilo.aguilar@gmail.com>
2014-03-20 00:01:05 +0100

    Binds Snapshot.Child() and Snapshot.NumChildren()

--- vix.go
+++ vix.go
@@ -2718,12 +2718,62 @@ type Snapshot struct {
 	IsReplayable bool
 }
 
-func (s *Snapshot) Child() {
+// This function returns the specified child snapshot.
+//
+// Parameters:
+//
+// index: Index into the list of snapshots.
+//
+// Remarks:
+//
+// * Snapshots are indexed from 0 to n-1, where n is the number of child
+//   snapshots. Use the function Snapshot.NumChildren() to get the value of n.
+// * This function is not supported when using the VMWARE_PLAYER provider.
+//
+// Since VMware Workstation 6.0
+func (s *Snapshot) Child(index int) (*Snapshot, error) {
+	var snapshotHandle C.VixHandle = C.VIX_INVALID_HANDLE
+	var err C.VixError = C.VIX_OK
 
+	err = C.VixSnapshot_GetChild(s.handle,
+		C.int(index),    //index
+		&snapshotHandle) //(output) A handle to the child snapshot.
+
+	if C.VIX_OK != err {
+		return nil, &VixError{
+			code: int(err & 0xFFFF),
+			text: C.GoString(C.Vix_GetErrorText(err, nil)),
+		}
+	}
+
+	snapshot := &Snapshot{
+		handle: snapshotHandle,
+	}
+
+	return snapshot, nil
 }
 
-func (s *Snapshot) NumChildren() {
+// This function returns the number of child snapshots of a specified snapshot.
+//
+// Remarks:
+//
+// * This function is not supported when using the VMWARE_PLAYER provider.
+//
+// Since VMware Workstation 6.0.
+func (s *Snapshot) NumChildren() (int, error) {
+	var err C.VixError = C.VIX_OK
+	var numChildren *C.int
 
+	err = C.VixSnapshot_GetNumChildren(s.handle, numChildren)
+
+	if C.VIX_OK != err {
+		return 0, &VixError{
+			code: int(err & 0xFFFF),
+			text: C.GoString(C.Vix_GetErrorText(err, nil)),
+		}
+	}
+
+	return int(*numChildren), nil
 }
 
 func (s *Snapshot) Parent() {
