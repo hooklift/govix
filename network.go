b9a66257fe2f2e04a7a869e970c63e58dcdb9996
Camilo Aguilar <camilo.aguilar@gmail.com>
2014-08-05 18:15:30 -0400

    Fixes godocs

--- network.go
+++ network.go
@@ -7,120 +7,83 @@ import (
 	"strings"
 )
 
-// VMware Fusion and Workstation
-// come with predefined virtual switches,
-// tailored for the most common uses cases.
+// VMware Fusion and Workstation come with predefined virtual switches, tailored
+// or the most common uses cases.
 //
-// This type defines the common networking
-// scenarios available for you to choose from,
-// when adding network adapters to your virtual
-// machines.
+// This type defines the common networking scenarios available for you to choose from,
+// when adding network adapters to your virtual machines.
 type NetworkType string
 
 const (
-	// Host-only networking provides a network
-	// connection between the virtual machine
-	// and the host computer, using a virtual
-	// Ethernet adapter that is visible to the
-	// host operating system. This approach can
-	// be very useful if you need to set up an
+	// Host-only networking provides a network connection between the virtual machine
+	// and the host computer, using a virtual Ethernet adapter that is visible to the
+	// host operating system. This approach can be very useful if you need to set up an
 	// isolated virtual network.
 	//
-	// If you use host-only networking, your
-	// virtual machine and the host-only
-	// adapter are connected to a private TCP/IP
-	// network. Addresses on this network are
+	// If you use host-only networking, your virtual machine and the host-only
+	// adapter are connected to a private TCP/IP network. Addresses on this network are
 	// provided by the VMware DHCP server.
 	//
 	// Source: https://www.vmware.com/support/ws55/doc/ws_net_configurations_hostonly.html
 	NETWORK_HOSTONLY NetworkType = "hostonly"
 
-	// If you want to connect to the Internet
-	// or other TCP/IP network using the host
-	// computer's dial-up networking or broadband
-	// connection and you are not able to give your
-	// virtual machine an IP address on the external
-	// network, NAT is often the easiest way to give
-	// your virtual machine access to that network.
+	// If you want to connect to the Internet or other TCP/IP network using the host
+	// computer's dial-up networking or broadband connection and you are not able
+	// to give your virtual machine an IP address on the external network, NAT
+	// is often the easiest way to give your virtual machine access to that network.
 	//
-	// If you use NAT, your virtual machine does not
-	// have its own IP address on the external network.
-	// Instead, a separate private network is set up
-	// on the host computer. Your virtual machine gets
-	// an address on that network from the VMware virtual
-	// DHCP server. The VMware NAT device passes network
-	// data between one or more virtual machines and
-	// the external network. It identifies incoming
-	// data packets intended for each virtual machine
-	// and sends them to the correct destination.
+	// If you use NAT, your virtual machine does not have its own IP address on
+	// the external network. Instead, a separate private network is set up
+	// on the host computer. Your virtual machine gets an address on that network
+	// from the VMware virtual DHCP server. The VMware NAT device passes network
+	// data between one or more virtual machines and the external network. It
+	// identifies incoming data packets intended for each virtual machine and
+	// sends them to the correct destination.
 	//
-	// If you select NAT, the virtual machine can use
-	// many standard TCP/IP protocols to connect to
-	// other machines on the external network.
-	// For example, you can use HTTP to browse Web
-	// sites, FTP to transfer files and Telnet to
-	// log on to other computers. In the default
-	// configuration, computers on the external network
-	// cannot initiate connections to the virtual machine.
-	// That means, for example, that the default
-	// configuration does not let you use the virtual
-	// machine as a Web server to send Web pages to
-	// computers on the external network.
+	// The NAT device is connected to the VMnet8 virtual switch. This means that
+	// virtual machines connected to the NAT network also use the VMnet8 virtual switch.
 	//
-	// The NAT device is connected to the VMnet8 virtual switch.
-	// Virtual machines connected to the NAT network also use
-	// the VMnet8 virtual switch.
-	//
-	// The NAT device acts as a DNS server for the virtual
-	// machines on the NAT network. Actually, the NAT device
-	// is a DNS proxy and merely forwards DNS requests
-	// from the virtual machines to a DNS server that is
-	// known by the host. Responses come back to the NAT
-	// device, which then forwards them to the virtual machines.
+	// The NAT device acts as a DNS server for the virtual machines on the NAT
+	// network. Actually, the NAT device is a DNS proxy and merely forwards DNS requests
+	// from the virtual machines to a DNS server that is known by the host.
+	// Responses come back to the NAT device, which then forwards them to the
+	// virtual machines.
 	//
 	// Source: https://www.vmware.com/support/ws55/doc/ws_net_configurations_nat.html
 	NETWORK_NAT NetworkType = "nat"
 
-	// If your host computer is on an Ethernet network,
-	// this is often the easiest way to give your virtual
-	// machine access to that network. Linux and Windows
-	// hosts can use bridged networking to connect to
-	// both wired and wireless networks.
+	// If your host computer is on an Ethernet network, this is often the easiest
+	// way to give your virtual machine access to that network. Linux and Windows
+	// hosts can use bridged networking to connect to both wired and wireless networks.
 	//
-	// If you use bridged networking, your virtual machine
-	// needs to have its own identity on the network.
-	// For example, on a TCP/IP network, the virtual
-	// machine needs its own IP address. Your network
-	// administrator can tell you whether IP addresses
-	// are available for your virtual machine and what
-	// networking settings you should use in the guest
-	// operating system. Generally, your guest operating
-	// system may acquire an IP address and other network
-	// details automatically from a DHCP server, or you may
-	// need to set the IP address and other details manually
-	// in the guest operating system.
+	// If you use bridged networking, your virtual machine needs to have its own
+	// identity on the network. For example, on a TCP/IP network, the virtual
+	// machine needs its own IP address. Your network administrator can tell you
+	// whether IP addresses are available for your virtual machine and what
+	// networking settings you should use in the guest operating system. Generally,
+	// your guest operating system may acquire an IP address and other network
+	// details automatically from a DHCP server, or you may need to set the IP
+	// address and other details manually in the guest operating system.
 	//
-	// If you use bridged networking, the virtual machine is
-	// a full participant in the network. It has access to
-	// other machines on the network and can be contacted
-	// by other machines on the network as if it were a
-	// physical computer on the network.
+	// If you use bridged networking, the virtual machine is a full participant
+	// in the network. It has access to other machines on the network and can be
+	// contacted by other machines on the network as if it were a physical computer
+	// on the network.
 	//
 	// Source: https://www.vmware.com/support/ws55/doc/ws_net_configurations_bridged.html
 	NETWORK_BRIDGED NetworkType = "bridged"
 
-	// The virtual networking components provided by VMware
-	// Workstation make it possible for you to create
-	// sophisticated virtual networks. The virtual networks
-	// can be connected to one or more external networks,
-	// or they may run entirely on the host computer.
+	// The virtual networking components provided by VMware Workstation make it
+	// possible for you to create sophisticated virtual networks. The virtual networks
+	// can be connected to one or more external networks, or they may run entirely
+	// on the host computer.
 	//
-	// If you choose for adding a custom network adapter
-	// you also have to provide the virtual switch to
-	// which it is going to be attached to.
+	// If you choose for adding a custom network adapter you also have to provide
+	// the virtual switch to which it is going to be attached to.
 	NETWORK_CUSTOM NetworkType = "custom"
 )
 
+// Represents the drivers supported by VMware
 type VNetDevice string
 
 const (
@@ -138,6 +101,7 @@ const (
 	NETWORK_DEVICE_VMXNET3 VNetDevice = "vmxnet3"
 )
 
+// The type of MAC addresses
 type MacAddressType string
 
 const (
@@ -152,13 +116,13 @@ const (
 	NETWORK_MACADDRESSTYPE_VPX MacAddressType = "vpx"
 )
 
+// A network adapter is usually attached to one virtual switch
 type NetworkAdapter struct {
 	// The identifier of the network adapter
 	Id string
 
-	// This field was made private while we decide whether or not to
-	// expose this field to users since it could cause some
-	// confusion.
+	// This field was made private while we decide whether or not to expose it
+	// to users since it could cause some confusion.
 	//
 	// Whether or not the adapter will be make present to the VM
 	present bool
@@ -169,62 +133,50 @@ type NetworkAdapter struct {
 	// The actual ethernet virtual hardware. e1000 by default
 	Vdevice VNetDevice
 
-	// Workstation 6 and higher only.
-	// Set to "true" to enable WakeOnLan functions
-	// Don't specify unless you really need it.
-	// "false" by default
+	// Workstation 6 and higher only. Set to "true" to enable WakeOnLan functions
+	// Don't specify unless you really need it. "false" by default
 	WakeOnPcktRcv bool
 
-	// Enables applications to seamlessly communicate
-	// when using bridged networking even when moving
-	// between networks. For example, communication between
-	// applications will continue seamlessly when you move
-	// from a wired network to a wireless network.
+	// Enables applications to seamlessly communicate when using bridged networking,
+	// even when the Host computer moves to another network. For example,
+	// communication between applications will continue seamlessly when you move
+	// your laptop from a wired network to a wireless network.
 	LinkStatePropagation bool
 
-	// Address type of the MAC
-	// by default it is "generated"
+	// Address type of the MAC by default it is "generated"
 	MacAddrType MacAddressType
 
-	// MAC Address
 	// Used only when MacAddrType is NETWORK_MACADDRESSTYPE_STATIC
 	// It also has to have a value within the MAC address range that
 	// is registered to VMware, Inc: 00:50:56:00:00:00 - 00:50:56:3F:FF:FF.
 	// Source: http://pubs.vmware.com/vsphere-4-esxi-installable-vcenter/index.jsp?topic=/com.vmware.vsphere.esxi_server_config.doc_41/esx_server_config/advanced_networking/c_setting_up_mac_addresses.html
 	MacAddress net.HardwareAddr
 
-	// If ConnType is NETWORK_CUSTOM,
-	// this field allows you to choose to which
-	// virtual switch you want to plug this
-	// virtual adapter to. Ex: vmnet2
+	// If ConnType is NETWORK_CUSTOM, this field allows you to choose to which
+	// virtual switch you want to plug this virtual adapter to. Ex: vmnet2
 	VSwitch VSwitch
 
 	// Whether or not the network adapter will be connected on boot
 	StartConnected bool
 
-	// Generated MAC Address by VMware
-	// Not need to set for adding network adapters
+	// Generated MAC Address by VMware. Not need to set for adding network adapters
 	GeneratedMacAddress net.HardwareAddr
 
-	//Generated MAC Address offset
-	// Not need to set for adding network adapters
+	// Generated MAC Address offset. Not need to set for adding network adapters
 	GeneratedMacAddressOffset string
 
-	// PCI Slot number generated by VMWare
-	// Not need to set for adding network adapters
+	// PCI Slot number generated by VMWare. Not need to set for adding network adapters
 	PciSlotNumber string
 }
 
 // Adds a network adapter to the virtual machine
 //
-// The "adapter" parameter is optional. If not
-// specified this function will add, by default,
-// a network adapter with NAT support; autogenerated
-// MAC address, and e1000 as network device.
+// The "adapter" parameter is optional. If not specified this function will add,
+// by default, a network adapter with NAT support; autogenerated MAC address,
+// and e1000 as network device.
 //
-// Be aware that the adapter will not show up in
-// the VMware Preferences user interface immediatelly.
-// Once the virtual machine is started the UI will pick it up.
+// Be aware that the adapter will not show up in the VMware Preferences user
+// interface immediatelly. Once the virtual machine is started the UI will pick it up.
 func (v *VM) AddNetworkAdapter(adapter *NetworkAdapter) error {
 	isVmRunning, err := v.IsRunning()
 	if err != nil {
@@ -444,7 +396,7 @@ func (v *VM) RemoveNetworkAdapter(adapter *NetworkAdapter) error {
 	return nil
 }
 
-// List network adapters attached to the virtual
+// List current network adapters attached to the virtual
 // machine.
 func (v *VM) NetworkAdapters() ([]NetworkAdapter, error) {
 	vmxPath, err := v.VmxPath()
