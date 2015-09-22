15c1838b00a23836f557053bbad280b5575ab8ee
Camilo Aguilar <camilo.aguilar@gmail.com>
2015-09-22 11:00:47 -0400

    Fixes leftovers from old repo

--- doc.go
+++ doc.go
@@ -29,8 +29,8 @@
 //
 // Example:
 //
-//  * OSX: export DYLD_LIBRARY_PATH=${GOPATH}/src/github.com/cloudescape/govix
-//  * Linux: export LD_LIBRARY_PATH=${GOPATH}/src/github.com/cloudescape/govix
+//  * OSX: export DYLD_LIBRARY_PATH=${GOPATH}/src/github.com/hooklift/govix
+//  * Linux: export LD_LIBRARY_PATH=${GOPATH}/src/github.com/hooklift/govix
 //  * Windows: append the path to the PATH environment variable
 //
 // Be aware that the previous example assumes $GOPATH only has a path set.
